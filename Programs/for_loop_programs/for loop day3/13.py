7d9cc00f22fddc10c771509bc1c38d8c36e2942e
kumarrahul771996 <kumarrahul771996@gmail.com>
2024-06-21 19:53:14 +0000

    done: for loop day3 hw

--- Programs/for_loop_programs/for loop day3/13.py
+++ Programs/for_loop_programs/for loop day3/13.py
@@ -2,8 +2,14 @@
 
 l = [[1,2,3], [4,5,6], [7,8,9]]
 
-output:-->
-
+# output:
    #internal = 6, 15, 24
-
    #external --> 45
+internal = []
+
+for i in l:
+   internal.append(sum(i))
+
+external = sum(internal)
+print(*internal) # * for removing round bracket symbol i.e []
+print(external)
--- Programs/for_loop_programs/for loop day3/14.py
+++ Programs/for_loop_programs/for loop day3/14.py
@@ -1,4 +1,4 @@
-# 14.wap to using this list get the below output
+# 14.wap using below list and get the below output
 
 
 l = ['sun flower', 'Lilly flower', 'Marigold flower', 'lion animal', 'tiger animal', 'eagle bird', 'snake animal', 'lotus flower', 'pigeon bird']
@@ -6,6 +6,18 @@ l = ['sun flower', 'Lilly flower', 'Marigold flower', 'lion animal', 'tiger anim
 # o/p:-->{'flower': ['sun', 'Lilly', 'Marigold', 'lotus'], 'animal': ['lion', 'tiger', 'snake'], 'bird': ['eagle', 'pigeon']}
 
 d = {}
+category_list = []
 
 for i in l:
-    # i.split()
+  category = i.split()[1] 
+  
+  if category not in d.keys():
+    for j in l:
+      if j.split()[1] ==  category:
+        if j.split()[0] not in category_list:
+          category_list.append(j.split()[0])
+    
+    d[category] = category_list
+    category_list = []
+
+print(d)
