24c34237262e9c3377fadcddf4223981e9584e22
kumarrahul771996 <kumarrahul771996@gmail.com>
2024-06-22 13:40:49 +0000

    add(interview qns, for loop day4)

--- Programs/for_loop_programs/for loop day3/13.py
+++ Programs/for_loop_programs/for loop day3/13.py
@@ -5,11 +5,24 @@ l = [[1,2,3], [4,5,6], [7,8,9]]
 # output:
    #internal = 6, 15, 24
    #external --> 45
-internal = []
 
+# solution
+'''
+internal = []
 for i in l:
    internal.append(sum(i))
-
 external = sum(internal)
-print(*internal) # * for removing round bracket symbol i.e []
+print(*internal) # * for removing round bracket i.e []
 print(external)
+'''
+# sir's code
+
+res = []
+for i in l:
+   sum_internal = 0
+   for j in i:
+      sum_internal += j
+   res.append(sum_internal)
+
+
+print(res);
---
+++ Programs/for_loop_programs/for loop day4/practice.py
@@ -0,0 +1,55 @@
+a = [1,-2,3,-4,7,-8,9,-11,12]
+
+# continue
+# for i in a:
+#     if i > 0:
+#         continue
+#     print(i)
+'''
+for i in a:
+    if i > 0:
+        break 
+    print(i)
+'''
+# x = 'good day wasted'
+
+# for i in x:
+#     if i == 'a':
+#         break
+#     print(i, end='')
+
+# y = 'programming part in class'
+
+# for i in y:
+#     if i in 'aeiou':
+#         continue
+#     print(i, end="")
+
+# print()
+
+# reversed
+# l = ['hello', 'hi', 'byebye']
+# for i in l:
+#     for j in reversed(i):
+#         print(j, end='')
+#     print()
+
+# enumerate
+# l = ['hello', 'hi', 'byebye']
+# for i in enumerate(l):
+#     print(i)
+
+# zip
+
+# l = ['hello', 'hi', 'byebye']
+# l2 = [3,1,4,7]
+# for i in zip(l,l2):
+#     print(i)
+
+# zip_longest
+from itertools import zip_longest
+l = ['hello', 'hi', 'byebye']
+l2 = [3,1,4,7]
+for i in zip_longest(l,l2):
+    print(i)
+
