d7096849e18be77c2b8ae043d4b722ffc172b4e3
Rahul Kumar <kumarrahul771996@gmail.com>
2024-07-03 08:54:11 +0000

    started file handling

--- Programs/hello.py
+++ Programs/hello.py
@@ -1,4 +1,11 @@
-for i in range(5):
-    for j in range(i):
-        print("*", end=' ')
-    print() 
+# sum of two nos
+
+# without using function
+# a=int(input("enter the first no."))
+# b=int(input("enter the second no."))
+# c=a+b
+# print(c)
+
+# with using function
+a=int(input("enter the first no."))
+b=int(input("enter the second no."))
