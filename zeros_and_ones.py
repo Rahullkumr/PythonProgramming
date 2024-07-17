1fc3544db67e5e1336e9ab30af685b8038c305e1
Rahullkumr <kumarrahul771996@gmail.com>
2024-07-17 22:54:54 +0530

    fix: zeros_and_ones program(first approach)

--- zeros_and_ones.py
+++ zeros_and_ones.py
@@ -1,12 +1,13 @@
 l = [1, 0, 1, 0, 1, 0]
 
 # first approach
-# l1,l2 = [],[]
-#   if i == 0:
-#     l1.append(i)
-#   else:
-#     l2.append(i)
-# print(l1+l2)
+l1,l2 = [],[]
+for i in l:
+  if i == 0:
+    l1.append(i)
+  else:
+    l2.append(i)
+print(l1+l2)
 
 # ===========================================
 
@@ -16,10 +17,10 @@ l = [1, 0, 1, 0, 1, 0]
 # ===========================================
 
 # third approach
-i = 0
-for j in range(len(l)):
-    if l[j] == 0:
-        l[i], l[j] = l[j], l[i]
-        i = i + 1
+# i = 0
+# for j in range(len(l)):
+#     if l[j] == 0:
+#         l[i], l[j] = l[j], l[i]
+#         i = i + 1
 
-print(l)
+# print(l)
