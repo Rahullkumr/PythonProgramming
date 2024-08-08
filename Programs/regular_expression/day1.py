a2fe8e947afbcf127e5f836defd7161e97a0574d
Rahullkumr <kumarrahul771996@gmail.com>
2024-08-08 13:05:54 +0530

    regular expression day one

--- Programs/regular_expression/day1.py
+++ Programs/regular_expression/day1.py
@@ -1,36 +1,40 @@
-"""Regular Expression"""
+# """Regular Expression"""
 
 import re
 s = 'good day'
 
-x = re.match('good', s)
-print(x)
-print(x.start())
-print(x.end())
-print(x.group())
+# # match
+# x = re.match('good', s)
+# print(x)
+# print(x.start())
+# print(x.end())
+# print(x.group())
 
+# # fullmatch
+# print(re.fullmatch('good day', s))
+# print(re.fullmatch('good', s))
 
-print(re.fullmatch('good day', s))
-print(re.fullmatch('good', s))
+# # search
+# var = re.search('d', s)
+# print(var)
 
-var = re.search('d', s)
-print(var)
-
+# sub
 x = re.sub('o','%',s,3)
 print(x)
 y = re.subn('o','#',s,4)
 print(y)
 
-x = re.split('o', s, 1)
-print(x)
+# # split
+# x = re.split('o', s, 1)
+# print(x)
 
-# findall
-s = '123good day888 guys'
+# # findall
+# s = '123good day888 guys'
 
-x = re.findall('[0-9]', s)
-print(x)
+# x = re.findall('[0-9]', s)
+# print(x)
 
-x = re.findall('[0-9]+', s)
-print(x)
+# x = re.findall('[0-9]+', s)
+# print(x)
 
-# \s \d \D \w(no special char) \W 
+# # \s \d \D \w(no special char) \W 
