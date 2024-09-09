fb118e7b30a361d43fc91b76bd144d2a8e57052e
Rahullkumr <kumarrahul771996@gmail.com>
2024-09-10 03:36:52 +0530

    chore: cleaned(matplotlib files)

--- library_matplotlib/day1.py
+++ library_matplotlib/day1.py
@@ -3,67 +3,87 @@ import numpy as np
 
 # matplotlib: john d hunter
 
-# line graph
-# """
-x = np.array([10,20,30,40])
+# LINE GRAPH
+"""
+x = np.array([10, 20, 30, 40])
 y = x ** 2
 
 plt.xlabel("x-axis")
 plt.ylabel("y-axis")
 plt.title("Normal line graph")
-plt.plot(x,y,marker="s",ls="dashdot",mfc="r", mec="b", ms=20, lw="15", color="m", label="Information")
+plt.plot(x,y,marker="*",ls="dashed",mfc="r",mec="b",ms=2,lw="1",color="m",label="Information")
 plt.legend(loc="upper right")
-plt.legend(loc="center")
+# plt.legend(loc="center")
+plt.show()
+"""
+
+"""
+markers = point of intersection of x and y axis.
+    "o",  # Circle              "s",  # Square                  "^",  # Triangle Up
+    "v",  # Triangle Down       "<",  # Triangle Left           ">",  # Triangle Right
+    "D",  # Diamond             "d",  # Thin Diamond            "p",  # Pentagon
+    "h",  # Hexagon1            "H",  # Hexagon2                "*",  # Star
+    "+",  # Plus                "x",  # Cross                   "X",  # Filled Cross
+    "|",  # Vertical Line       "-",  # Horizontal Line         "_",  # Underscore (thin horizontal line)
+    "1",  # Tri-down            "2",  # Tri-up                  "3",  # Tri-left
+    "4",  # Tri-right           ".",  # Point (tiny dot)        ",",  # Pixel (smaller than a point)            
+    "None",  # No marker        None,  # No marker              " "  # No marker
+
+ls = line styles
+    ("solid", "-"),            ("dashed", "--"),        ("dashdot", "-."),               
+    ("dotted", ":"),           ("none", "None"),     
+
+
+mfc     = marker face color (the inside of the square markers)
+mec     = marker edge color (the border of the square markers)
+ms      = marker size
+lw      = line width
+color   = line color
+label   = label to be used in the legend
+"""
+
+# BAR GRAPH
+# """
+lang = ['Python', 'Java', 'C', 'C++', 'Js']
+rating = [60, 30, 80, 15, 45]
+c = ['w', 'w', 'w', 'w', 'w']
+
+plt.ylabel("rating")
+plt.xlabel("languages")
+plt.title("Language info")
+plt.bar(lang, rating, color=c, width=0.5, align="edge", label="language", edgecolor="olive", linewidth=2)
+plt.legend()
 plt.show()
 # """
+'''
+colors = 
+    ("blue", "b")               ("green", "g")                  ("red", "r")            ("cyan", "c")
+    ("magenta", "m")            ("yellow", "y")                 ("black", "k")          ("white", "w")
+    ("gray", "gray")            ("darkgray", "darkgray")        ("orange", "orange")    ("pink", "pink")
+    ("purple", "purple")        ("brown", "brown")              ("lime", "lime")        ("olive", "olive"), etc
+        
+    also you can use:
+        Hexadecimal strings like "#f500f5"
+        RGB tuples like (255, 255, 1)
+        RGBA tuples for transparency, like (0.1, 0.2, 0.5, 0.8) ()
+        
 
-# bar graph
-"""
+'''
+
+
+# hw
+'''
 l = ['Python', 'Java', 'C', 'C++', 'Js']
 r = [60,30,80,15,45]
 c = ['w', 'w', 'w', 'w', 'w']
+cc = ['r', 'g', 'b','y', 'm']
+
+rr = [30,15,40,7,22]
 plt.ylabel("rating")
 plt.xlabel("languages")
 plt.title("Language info")
 plt.bar(l,r,color=c,width=0.5, align="edge", label="language",edgecolor="b", linewidth=2)
+plt.bar(l,rr,color=cc,width=0.5, align="edge", label="language",edgecolor="b", linewidth=2)
 plt.legend()
 plt.show()
-"""
-# hw
-
-# l = ['Python', 'Java', 'C', 'C++', 'Js']
-# r = [60,30,80,15,45]
-# c = ['w', 'w', 'w', 'w', 'w']
-# cc = ['r', 'g', 'b','y', 'm']
-#
-# rr = [30,15,40,7,22]
-# plt.ylabel("rating")
-# plt.xlabel("languages")
-# plt.title("Language info")
-# plt.bar(l,r,color=c,width=0.5, align="edge", label="language",edgecolor="b", linewidth=2)
-# plt.bar(l,rr,color=cc,width=0.5, align="edge", label="language",edgecolor="b", linewidth=2)
-# plt.legend()
-# plt.show()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+'''
--- library_matplotlib/day3.py
+++ library_matplotlib/day3.py
@@ -1,65 +1,54 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-# a = [10,20,30,40,50]
-# b = [11,25,35,33,55]
-# size = [15,16,17,18,19]
-#
-# plt.xlabel('x value number',fontsize=15)
-# plt.ylabel('y value number',fontsize=20)
-# plt.title('Scatter Graph')
-# plt.scatter(a,b,sizes=size,color=['r','g','y','c','m'],marker='*',edgecolors='green',label='Graph',alpha=0.2)
-# plt.legend()
-# plt.colorbar()
-# plt.show()
+# SCATTER GRAPH
+"""
+a               = [10,20,30,40,50]
+b               = [11,25,35,33,55]
+marker_size     = [50,60,70,80,90]
+
+plt.xlabel('x value number',fontsize=15)
+plt.ylabel('y value number',fontsize=20)
+plt.title('Scatter Graph')
+plt.scatter(
+    a,b,
+    sizes=marker_size,
+    color=['r','g','y','c','m'],
+    marker='*',
+    edgecolors='green',
+    label='Scatter Graph',
+    alpha=0.2
+)
+plt.legend()
+plt.colorbar()
+plt.show()
+"""
+
 
 # make a graph full of dots
+"""
+a = [1,6,4,8,3,8,2,0,4,8,4,7,4,6,6,3,9,2]
+b = [6,9,8,7,9,5,5,3,7,4,8,4,8,3,8,4,6,8]
+c = ['r','g','b','y','m','b','b','y','m','b','b','y','m','b','b','y','m','b']
+plt.xlabel('x',fontsize=15)
+plt.ylabel('y',fontsize=20)
+plt.title('Scatter Graph')
+plt.scatter(a,b,marker='+',color=c)
+plt.show()
+"""
 
-# a = [1,6,4,8,3,8,2,0,4,8,4,7,4,6,6,3,9,2]
-# b = [6,9,8,7,9,5,5,3,7,4,8,4,8,3,8,4,6,8]
-# c = ['r','g','b','y','m','b','b','y','m','b','b','y','m','b','b','y','m','b']
-# plt.xlabel('x',fontsize=15)
-# plt.ylabel('y',fontsize=20)
-# plt.title('Scatter Graph')
-# plt.scatter(a,b,marker='+',color=c)
-# plt.show()
 
-# stack plot
 
-s = [10,20,30,40]
-x = [11,12,13,14]
-y = [12,13,14,15]
-z = [13,14,15,16]
-q = ['area1','area2','area3']
-plt.stackplot(s,x,y,z,labels=q,colors=['r','m','c'])
+
+# STACK PLOT
+# """
+x_axis = [1,2,3,4]
+y1 = [150, 200, 250, 300]
+y2 = [100, 150, 200, 250]
+y3 = [50, 100, 150, 200]
+label_names = ['area1','area2','area3']
+plt.grid(axis="y")
+plt.stackplot(x_axis,y1,y2,y3, labels=label_names,colors=['r','m','c'])
 plt.legend()
 plt.show()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# """
