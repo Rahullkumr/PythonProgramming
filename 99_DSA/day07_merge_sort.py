6ec4d0ac0dcffa44b959acca97aa78bb69ad862b
Rahullkumr <kumarrahul771996@gmail.com>
2024-11-21 00:26:23 +0530

    fix: filenames

--- 99_DSA/day07_merge_sort.py
+++ 99_DSA/day07_merge_sort.py
@@ -1,24 +1,25 @@
 # merge sort
 def merge(a, l, r):
-    i,j,k = 0,0,0
+    i, j, k = 0, 0, 0
     while i < len(l) and j < len(r):
         if l[i] < r[j]:
             a[k] = l[i]
-            i, k = i+1, k+1
+            i, k = i + 1, k + 1
 
         else:
             a[k] = r[j]
-            j, k = j+1, k+1
+            j, k = j + 1, k + 1
 
     while i < len(l):
         a[k] = l[i]
-        i, k = i+1, k+1
+        i, k = i + 1, k + 1
 
     while j < len(r):
         a[k] = r[j]
-        j, k = j+1, k+1
+        j, k = j + 1, k + 1
 
-arr = [1,8,7,5,18,3,2]
+
+arr = [1, 8, 7, 5, 18, 3, 2]
 # divide(arr)
 
 
